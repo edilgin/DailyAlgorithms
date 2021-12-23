2160f85fce01a446118d0c3514d779138c89dac2
fehim ornek <fehimornek@gmail.com>
2021-12-23 22:07:03 +0300

    solution with a lot of if statements

---
+++ check_substring.py
@@ -0,0 +1,25 @@
+"""
+Q: You are given two strings. Check if the first string is a subsequence of the other string. A subsequence of a string
+   is when the new string is formed by deleting some or none elements of the original list. Important thing is that
+   characters positions relative to each other must be preserved
+"""
+
+def isSubsequence(s, t):
+    idx_s = 0
+    idx_t = 0
+    while True:
+        if s[idx_s] == t[idx_t]:
+            idx_s += 1
+            idx_t += 1
+        elif s[idx_s] != t[idx_t]:
+            idx_t += 1
+        if idx_s == len(s):
+            return True
+        elif idx_s < len(s) and idx_t == len(t):
+            return False
+
+flag = isSubsequence("abc","ahbgdc")
+print("is substring:", flag)
+flag = isSubsequence("axc","ahbgdc")
+print("is substring:", flag)
+
