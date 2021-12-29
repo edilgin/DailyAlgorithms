95fedbf75b41adb8048c8e5d84eb17d98b0575d2
fehim ornek <fehimornek@gmail.com>
2021-12-29 18:44:05 +0300

    added the question part

--- q13_movezeros.py
+++ q13_movezeros.py
@@ -1,3 +1,8 @@
+"""
+Q: You are given an array like [1,4,0,7,0,2,9], create a function which moves zeros to the end of the list while
+   maintaining the order of the list like this: [1,4,7,2,9,0,0]
+"""
+
 def moveZeros(arr):
     sortedArr = []
     end_index = 0
